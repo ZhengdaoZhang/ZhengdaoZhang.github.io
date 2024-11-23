97b9d2788da2afbb00f9a03e26bd63c60f571a58
ZhengdaoZhang <zzd1645@gmail.com>
2024-11-23 10:43:13 +0800

    2.3

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,5 @@
 
 export const loadImage = (url) => {
-
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.onload = () => {
@@ -9,7 +8,7 @@ export const loadImage = (url) => {
         img.onerror = (e) => {
             reject(e)
         }
-        img.src = url;
+        img.src = url
     })
 }
 
@@ -22,5 +21,25 @@ export const getImageInfo = async (url) => {
         });
     })
     const metaData = await promise
-    return {img,metaData};
+    return { img, metaData }
+}
+
+export const canvasToImage = (canvas, MIMEType = "image/png") => {
+    return new Promise((resolve) => {
+        canvas.toBlob((blob) => {
+            const newImg = new Image()
+            const url = URL.createObjectURL(blob)
+            newImg.onload = () => {
+                // 不再需要读取该 blob，因此释放该对象 
+                // URL.revokeObjectURL(url)
+                resolve(newImg)
+            };
+            newImg.src = url
+        }, MIMEType)
+    })
+}
+
+export const getBaseScaleRate = (width, heihgt, baseSize = 1080) => {
+    const shortSide = width > heihgt ? heihgt : width;
+    return shortSide / baseSize
 }
